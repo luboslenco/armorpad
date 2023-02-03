56a9550b8c252470c86a9f5e9023060d00241fcb
luboslenco <lubos.lenco@gmail.com>
2023-02-03 12:29:54 +0100

    Use IDLE_SLEEP flag

--- kincflags.js
+++ kincflags.js
@@ -4,3 +4,6 @@ flags.name = 'ArmorPad';
 flags.package = 'org.armorpad';
 flags.with_nfd = true;
 flags.with_tinydir = true;
+flags.on_project_created = function(project) {
+	project.addDefine('IDLE_SLEEP');
+}
